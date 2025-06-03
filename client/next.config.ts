be361e040e19a0a37d7e80f215488b1b130231ba
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-06-02 20:41:15 -0500

    Trying to fix err

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,11 +5,11 @@ const nextConfig: NextConfig = {
   rewrites: async () => {
     return [
       {
-        source: "/api/index",
+        source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:5328/api/:path*"
-            : "/api/",
+            : "/api/index",
       },
     ];
   },
