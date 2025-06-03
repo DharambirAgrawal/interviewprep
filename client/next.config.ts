5a03b4accb1a5b7351528df7647181ebf91f9fe0
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-06-02 20:57:22 -0500

    Updated some caviats

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,15 +1,32 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   rewrites: async () => {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination:
+//           process.env.NODE_ENV === "development"
+//             ? "http://127.0.0.1:5328/api/:path*"
+//             : "/api/index",
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  rewrites: async () => {
+  async rewrites() {
     return [
       {
         source: "/api/:path*",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:5328/api/:path*"
-            : "/api/index",
+            : "/api/index/:path*", // ✅ preserve full path in production
       },
     ];
   },
