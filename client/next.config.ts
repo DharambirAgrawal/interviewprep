8026f5fdc20d23deca7354e29b5dadee421df2db
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-06-03 16:40:52 -0500

    Updated the client env and files

--- client/next.config.ts
+++ client/next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:5328/api/:path*"
-            : "/api/index",
+            : "/api/",
       },
     ];
   },
