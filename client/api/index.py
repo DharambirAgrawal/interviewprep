69bc3dcedc35fc7af856826366cd583d29e63039
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-06-03 15:05:27 -0500

    Added python files

--- client/api/index.py
+++ client/api/index.py
@@ -11,8 +11,22 @@
 #     return 'About'
 
 from flask import Flask
+import tempfile
 app = Flask(__name__)
 
-@app.route("/api/python")
+@app.route("/api/test")
 def hello_world():
     return "<p>Hello, World from flask!</p>"
+
+
+@app.post("/api/resume-text")
+async def resume_api(file: UploadFile = File(...)):
+    with tempfile.NamedTemporaryFile(delete=False, suffix=file.filename[-5:]) as tmp:
+        content = await file.read()
+        tmp.write(content)
+        tmp_path = tmp.name
+    try:
+        resume_text = extract_resume_text(tmp_path)
+        return {"text": resume_text}
+    finally:
+        os.remove(tmp_path)
