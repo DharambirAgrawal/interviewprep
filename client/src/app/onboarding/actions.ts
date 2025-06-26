7663f36d0c02686c8b8c54578dbe17d7b4749982
Dharambir Agrawal <dev.dharambir@gmail.com>
2025-06-26 09:07:26 -0500

    Updated the actions

--- client/src/app/onboarding/actions.ts
+++ client/src/app/onboarding/actions.ts
@@ -28,7 +28,7 @@ export async function handleResumeUpload(file: File) {
   }
 }
 
-// connecting to python backend for onboarding
+// connecting to python backend
 export async function handleOnboardingAction(data: any) {
   try {
     // Append file (resume)
